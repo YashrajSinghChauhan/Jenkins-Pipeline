37011b714e0be630353f4bc1386e5fa01b56f68a
harshitaYadav2003 <harshitaYadav2003>
2024-10-24 20:03:28 +0530

    Jenkins Build Commit by Yash

--- src/main/java/com/jenkins/JenkinsProject/JenkinsProjectApplication.java
+++ src/main/java/com/jenkins/JenkinsProject/JenkinsProjectApplication.java
@@ -1,14 +1,22 @@
 package com.jenkins.JenkinsProject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsProjectApplication {
-
+	
 	public static void main(String[] args) {
 		System.out.println("Jenkins Project Execution Started...");
+		System.out.println("Java Version: " + System.getProperty("java.version"));
+	   // checkMavenVersion();
 		SpringApplication.run(JenkinsProjectApplication.class, args);
+		
 	}
+	 
 
 }
